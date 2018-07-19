18a925ce0b1d8c248ebda63e7991cccef8e3ea68
Bob Meijwaard <bmeijwaard@gmail.com>
2018-07-19 11:11:11 +0200

    adjusted migrations to no thumbnails

--- TheBlogApi/Migrations/20180719091034_no-thumbnail.Designer.cs
+++ TheBlogApi/Migrations/20180719091034_no-thumbnail.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheBlogApi.Data.Context;
 
 namespace TheBlogApi.Migrations
 {
     [DbContext(typeof(SqlContext))]
-    partial class SqlContextModelSnapshot : ModelSnapshot
+    [Migration("20180719091034_no-thumbnail")]
+    partial class nothumbnail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TheBlogApi/Migrations/20180719091034_no-thumbnail.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheBlogApi.Migrations
+{
+    public partial class nothumbnail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ThumbnailUrl",
+                table: "Photos",
+                newName: "FileName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FileName",
+                table: "Photos",
+                newName: "ThumbnailUrl");
+        }
+    }
+}
